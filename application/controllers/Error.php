ab4d109636e53acc413c8fcc1237dde368e30ee5
cuiweifeng <cuiweifeng@yidian-inc.com>
2021-02-01 19:51:07 +0800

    update:base

--- application/controllers/Error.php
+++ application/controllers/Error.php
@@ -1,12 +1,26 @@
 <?php
-
-
 use Yaf\Controller_Abstract;
 
 class ErrorController extends Controller_Abstract
 {
-    public function errorAction($exception) {
-        var_dump($exception);
-        return false;
+
+    public function errorAction($exception)
+    {
+        switch ($exception->getCode()) {
+            // 404
+            //case Yaf\Registry::get(ERROR_CONFIG)->NOT_FOUND->code:
+            case YAF\ERR\NOTFOUND\MODULE:
+            case YAF\ERR\NOTFOUND\CONTROLLER:
+            case YAF\ERR\NOTFOUND\ACTION:
+            case YAF\ERR\NOTFOUND\VIEW:
+                // log
+                echo 'error';
+                return false;
+                break;
+            // 错误页面展示错误
+            default:
+                // log
+                return false;
+        }
     }
 }
--- application/modules/base/Base.php
+++ application/modules/base/Base.php
@@ -1,11 +1,57 @@
 <?php
-
-namespace base;
+namespace App\Base;
 
 use Yaf\Controller_Abstract;
+use App\Presenters\PresenterInterface;
+use App\Library\Http\Response;
+use Psr\Http\Message\ResponseInterface;
 
-class Base extends Controller_Abstract
+abstract class BaseController extends Controller_Abstract
 {
-    public function init() {
+
+    /**
+     * Headers.
+     *
+     * <pre>
+     * [
+     * 'content-type' => 'application/json;charset=utf-8'
+     * ]
+     * </pre>
+     *
+     * @var array
+     */
+    protected $headers = [];
+
+    /**
+     * 添加 header.
+     *
+     * @param string $name            
+     * @param mixed $value            
+     *
+     * @return $this
+     */
+    public function header(string $name, $value)
+    {
+        $this->headers[$name] = $value;
+        
+        return $this;
+    }
+
+    /**
+     * 获取设置的 headers.
+     *
+     * @return array
+     */
+    public function getHeaders()
+    {
+        return $this->headers;
+    }
+
+    public function returnJson($status, $msg, $data = [])
+    {
+        $res = $this->getResponse();
+        $res->setHeader("Content-Type", "application/json;charset=utf-8");
+        $data = Yaf_Registry::get('utils')->returnMsg($status, $msg, $data);
+        return $res->setBody($data);
     }
 }
