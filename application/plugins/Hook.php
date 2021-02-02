46be096a73894b2d94c8e214bb76edce44b7c61b
cuiweifeng <cuiweifeng@yidian-inc.com>
2021-02-02 16:13:24 +0800

    update: hook增加post，request兼容json形式

--- application/plugins/Hook.php
+++ application/plugins/Hook.php
@@ -15,6 +15,24 @@ class Hook extends \Yaf\Plugin_Abstract {
 	}
 
 	public function preDispatch(\Yaf\Request_Abstract $request, \Yaf\Response_Abstract $response) {
+		//处理$_POST, $_REQUEST请求,兼容application/json形式
+		if(!$_POST && $request->isPost()) {
+            $jsonPost = file_get_contents("php://input");
+            if($jsonPost) {
+				$_POST = json_decode($jsonPost, true);
+				$ini = ini_get('request_order');
+				if($ini) {
+					$arrIni = str_split($ini, 1);
+					foreach($arrIni as $type) {
+						if(strtoupper($type) === 'G') {
+							$_REQUEST = array_merge($_REQUEST, $_GET);
+						}elseif(strtoupper($type) === 'P') { 
+							$_REQUEST = array_merge($_REQUEST, $_POST);
+						}
+					}
+				}
+            }
+		}
 	}
 
 	public function postDispatch(\Yaf\Request_Abstract $request, \Yaf\Response_Abstract $response) {
